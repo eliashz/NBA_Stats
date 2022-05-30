653f2f7526e93705d819183e7265bd3f679f4aab
eliashz <eliashz@gmail.com>
2022-05-30 16:51:39 +0200

    update escudos

--- js/scriptteam.js
+++ js/scriptteam.js
@@ -1,5 +1,16 @@
-import { fetchData } from "../modules/fetch.js";
+import { fetchData } from "../modules/fetchData.js";
+const este = ['7.png', '8.png', '10.png', '11.png', '13.png', '14.png', '15.png', '18.png', '19.png', '21.png', '24.png', '25.png', '26.png', '27.png', '29.png',];
+const oeste = [];
 
 //const teams = await fetchData('https://www.balldontlie.io/api/v1/teams');
 
-console.log(teams.data)
+//console.log(teams.data)
+
+const fragment = document.createDocumentFragment();
+
+
+
+
+
+const shieldsDiv = document.querySelector('.shield');
+shieldsDiv.appendChild(fragment);
