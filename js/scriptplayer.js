a21906ee919fcbacbf305ff02dc832c6e9e2691d
eliashz <eliashz@gmail.com>
2022-06-23 19:35:55 +0200

    playes done

--- js/scriptplayer.js
+++ js/scriptplayer.js
@@ -24,58 +24,57 @@ const selectYear = () => {
 const showPlayerInfo = async (player, year=2021) => {
     
     let playerById = await fetchData(`https://www.balldontlie.io/api/v1/season_averages?season=${year}&player_ids[]=${player.id}`);
+    console.log(playerById.data.length)
 
-     result.innerHTML = '';
-    //document.querySelector('.buscador').innerHTML = '';
-    document.querySelector('section').innerHTML = '';
-    //document.querySelector('.teams').innerHTML = '';
-    //document.querySelector('.container2').innerHTML = '';
+    if (playerById.data.length >= 1){
+        result.innerHTML = '';
+        document.querySelector('.buscador').innerHTML = '';
 
+        const fragment = document.createDocumentFragment();
+        const template = document.querySelector('#template-team').content;
 
+        const playerDiv = template.cloneNode(true);
 
-    const fragment = document.createDocumentFragment();
-    const template = document.querySelector('#template-team').content;
+        playerDiv.querySelector('img').src += `${player.team.id}.png`;
+        playerDiv.querySelector('img').alt = player.team.full_name;
 
-    const playerDiv = template.cloneNode(true);
+        playerDiv.querySelector('#team').innerHTML = player.first_name + ' ' + player.last_name;
+        playerDiv.querySelector('.name').textContent = `POSICION: ${player.position}`; 
+        playerDiv.querySelector('.position').textContent = `Altura: ${inchesToCm(player.height_feet, player.height_inches)} | Peso: ${poundsToKg(player.weight_pounds)}`
 
-    playerDiv.querySelector('img').src += `${player.team.id}.png`;
-    playerDiv.querySelector('img').alt = player.team.full_name;
+        fragment.appendChild(playerDiv);
 
-    playerDiv.querySelector('#team').innerHTML = player.first_name + ' ' + player.last_name;
-    playerDiv.querySelector('.name').textContent = `POSICION: ${player.position}`; 
-    playerDiv.querySelector('.position').textContent = `Altura: ${inchesToCm(player.height_feet, player.height_inches)} | Peso: ${poundsToKg(player.weight_pounds)}`
-
-    fragment.appendChild(playerDiv);
-
-    const playersDiv = document.querySelector('header');
-    playersDiv.appendChild(fragment);
+        const playersDiv = document.querySelector('header');
+        playersDiv.appendChild(fragment);
     
     //Template de la tabla
-    
-    const fragment2 = document.createDocumentFragment();
-    const template2 = document.querySelector('#template-table').content;
-    
-    const tableDiv = template2.cloneNode(true);
-    
-    tableDiv.querySelector('tbody');
-    
-    const tbody = tableDiv.querySelector('tbody');
-    const tr = document.createElement('tr');
+        const fragment2 = document.createDocumentFragment();
+        const template2 = document.querySelector('#template-table').content;
+        
+        const tableDiv = template2.cloneNode(true);
+        
+        tableDiv.querySelector('tbody');
+        
+        const tbody = tableDiv.querySelector('tbody');
+        const tr = document.createElement('tr');
 
-    showTable(playerById.data[0].season, tbody, tr);
-    showTable(playerById.data[0].games_played, tbody, tr);
-    showTable(playerById.data[0].min, tbody, tr);
-    showTable(playerById.data[0].pts, tbody, tr);
-    showTable(playerById.data[0].reb, tbody, tr);
-    showTable(playerById.data[0].ast, tbody, tr);
-    showTable(playerById.data[0].stl, tbody, tr);
-    showTable(playerById.data[0].blk, tbody, tr);
-    showTable(playerById.data[0].turnover, tbody, tr);
+        showTable(playerById.data[0].season, tbody, tr);
+        showTable(playerById.data[0].games_played, tbody, tr);
+        showTable(playerById.data[0].min, tbody, tr);
+        showTable(playerById.data[0].pts, tbody, tr);
+        showTable(playerById.data[0].reb, tbody, tr);
+        showTable(playerById.data[0].ast, tbody, tr);
+        showTable(playerById.data[0].stl, tbody, tr);
+        showTable(playerById.data[0].blk, tbody, tr);
+        showTable(playerById.data[0].turnover, tbody, tr);
 
-    fragment2.appendChild(tableDiv);
+        fragment2.appendChild(tableDiv);
 
-    const tablesDiv = document.querySelector('section');
-    tablesDiv.appendChild(fragment2);
+        const tablesDiv = document.querySelector('section');
+        tablesDiv.appendChild(fragment2);
+    } else {
+        result.textContent = player.first_name + ' ' + player.last_name + ' no jugó el año ' + year + '.';
+    }
 }
 
 const searchPlayer = async () => {
@@ -84,6 +83,7 @@ const searchPlayer = async () => {
 
     if (playerData.data.length == 0) { // La búsqueda no da ningún resultado.
         result.textContent = 'Jugador no encontrado.'
+        player.value = ''; //Vacía el input después de una búsqueda
     } else if (playerData.data.length === 1) { // Las búsqueda da un resultado -> imprimir estadísticas 
         showPlayerInfo(playerData.data[0], year);
     } else if (player) { // La búsqueda da muchos resultados -> el usuario puede seleccionar de una lista
@@ -100,7 +100,6 @@ const searchPlayer = async () => {
             playerDiv.addEventListener('click', selectPlayer);
         }
     }
-    player.value = ''; //Vacía el input después de una búsqueda
 }
 
 const playerFromTeamList = localStorage.getItem('playerSelected');
